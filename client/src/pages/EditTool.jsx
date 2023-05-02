8ad19a808b5d0a1d3059c56141d8bfd474c35fa0
CodyConck <codyconck@gmail.com>
2023-05-02 17:05:24 -0500

    updated home and edit tools pages, returning values to console onSubmit

--- client/src/pages/EditTool.jsx
+++ client/src/pages/EditTool.jsx
@@ -1,7 +1,64 @@
 // import React from 'react'
+import { Card, CardBody } from "reactstrap";
+import { Formik, Form, FormItem, FormSubmit } from "../components/Form";
+// import { useQueryClient } from "@tanstack/react-query";
+// import { useApp } from "../hooks/index.js";
 
 const EditTool = () => {
-  return <div>EditTool</div>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  // const app = useApp();
+  // const queryClient = useQueryClient();
+
+  // async function onSubmit(data, formik) {
+  //   try {
+  //     const result = await app.service("api/tools").create(data);
+  //     console.log(result);
+  //     queryClient.invalidateQueries(["api/tools"]);
+  //     formik.resetForm();
+  //   } catch (error) {
+  //     console.error(error);
+  //     if (error.errors) {
+  //       formik.setErrors(error.errors);
+  //     }
+  //   }
+  // }
+
+  return (
+    <Card>
+      <CardBody>
+        <Formik
+          onSubmit={onSubmit}
+          initialValues={{
+            search: undefined,
+            clientId: undefined,
+            name: undefined,
+            description: undefined,
+            size: undefined,
+          }}
+        >
+          <Form>
+            <FormItem name="search" label="Search Tools" />
+            <FormSubmit>Search</FormSubmit>
+            <FormItem name="clientId" label="Client ID" />
+            <FormItem name="name" label="Name" />
+            <FormItem
+              name="description"
+              type="textarea"
+              rows="4"
+              label="Description"
+            />
+            <FormItem name="size" type="radio" value="sm" label="Small" />
+            <FormItem name="size" type="radio" value="lg" label="Large" />
+            <FormItem name="size" type="radio" value="xl" label="Extra Large" />
+            <FormSubmit>Update</FormSubmit>
+          </Form>
+        </Formik>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default EditTool;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,31 @@
 // import React from 'react'
+import { FormItem, Formik, Form, FormSubmit } from "../components/Form.jsx";
+import { Card, CardBody } from "reactstrap";
+
+import "../styles/theme.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Card>
+      <CardBody>
+        <Formik
+          onSubmit={onSubmit}
+          initialValues={{
+            employeeID: "",
+          }}
+        >
+          <Form>
+            <FormItem name="employeeID" type="id" label="Employee ID" />
+            <FormSubmit>Submit</FormSubmit>
+          </Form>
+        </Formik>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default Home;
