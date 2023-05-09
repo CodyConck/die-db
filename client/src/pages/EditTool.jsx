d243c8f70b84e586b98c7ca91158d0eaa8bad170
CodyConck <codyconck@gmail.com>
2023-05-09 13:50:52 -0500

    updated edittools to try to update a tool, was unsussessful

--- client/src/pages/EditTool.jsx
+++ client/src/pages/EditTool.jsx
@@ -1,6 +1,9 @@
 // import React from 'react'
-import { Table } from "reactstrap";
-import { useQuery } from "@tanstack/react-query";
+// import { Table } from "reactstrap";
+import { useState } from "react";
+import { Card, CardBody } from "reactstrap";
+import { Formik, Form, FormItem, FormSubmit } from "../components/Form";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import { useApp } from "../hooks/index.js";
 import { useParams } from "react-router-dom";
 
@@ -10,30 +13,82 @@ const EditTool = () => {
   const { isLoading, data } = useQuery(["api/tools", id], () => {
     return app.service("api/tools").get(id);
   });
+
+  const [clientName, setClientName] = useState(data?.clientName || "");
+  const [toolName, setToolName] = useState(data?.toolName || "");
+  const [description, setDescription] = useState(data?.description || "");
+  const [size, setToolSize] = useState(data?.size || "");
+
+  const { mutate } = useMutation((updatedTool) => {
+    return app.service("api/tools").update(id, updatedTool);
+  });
+
+  const updateTool = (event) => {
+    event.preventDefault();
+    mutate({ clientName, toolName, description, size });
+  };
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
+
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Client Name</th>
-          <th>Tool Name</th>
-          <th>Size</th>
-          <th>Description</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr key={data._id}>
-          <td>{data._id}</td>
-          <td>{data.clientName}</td>
-          <td>{data.toolName}</td>
-          <td>{data.size}</td>
-          <td>{data.description}</td>
-        </tr>
-      </tbody>
-    </Table>
+    <Card>
+      <CardBody>
+        <Formik
+          onSubmit={updateTool}
+          initialValues={{
+            clientName: `${data.clientName}`,
+            toolName: `${data.toolName}`,
+            description: `${data.description}`,
+            size: `${data.size}`,
+          }}
+        >
+          <Form>
+            <FormItem name="_id" label="Tool ID" value={data._id} />
+            <FormItem
+              name="clientName"
+              label="Client Name"
+              onChange={(event) => setClientName(event.target.value)}
+            />
+            <FormItem
+              name="toolName"
+              label="Tool Name"
+              onChange={(event) => setToolName(event.target.value)}
+            />
+            <FormItem
+              name="description"
+              type="textarea"
+              rows="4"
+              label="Description"
+              onChange={(event) => setDescription(event.target.value)}
+            />
+            <FormItem
+              name="size"
+              type="radio"
+              value="sm"
+              label="Small"
+              onChange={(event) => setToolSize(event.target.value)}
+            />
+            <FormItem
+              name="size"
+              type="radio"
+              value="lg"
+              label="Large"
+              onChange={(event) => setToolSize(event.target.value)}
+            />
+            <FormItem
+              name="size"
+              type="radio"
+              value="xl"
+              label="Extra Large"
+              onChange={(event) => setToolSize(event.target.value)}
+            />
+            <FormSubmit>Update</FormSubmit>
+          </Form>
+        </Formik>
+      </CardBody>
+    </Card>
   );
 };
 
